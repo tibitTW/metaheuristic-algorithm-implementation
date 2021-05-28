7f1f1b8c67f5e4fff19ffe764338ccff5a223069
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-05-28 18:09:20 +0800

    place parameters to singla file

---
+++ gaconfig.h
@@ -0,0 +1,9 @@
+// pop-size should be even
+#define POPSIZE 20
+#define CHROMOSOME_LENGTH 10
+#define ITERATION_COUNT 100
+
+// during iteration
+#define SELECTION_COUNT 4
+#define CROSSOVER_RATE 0.6
+#define MUTATION_RATE 0.6
