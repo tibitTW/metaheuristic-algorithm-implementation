0e16503de47aacb0e12b2c720483d6e0599e11ef
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-06-08 07:58:20 +0800

    .

--- ACO/test.c
+++ ACO/test.c
@@ -1,8 +1,18 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 int main() {
-    { printf("Hello world"); }
+    printf("%f", pow(1.1, 100));
+    double a = 1.1;
+    for (int i = 0; i < 100; i++) {
+        printf("%10.5f ", a);
+        a *= 1.1;
+        if (i % 5 == 0)
+            printf("\n");
+    }
 
     return 0;
 }
