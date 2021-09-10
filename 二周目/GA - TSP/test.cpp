77361186658be8bbe8f5cba768cf5148bea6b593
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-09-10 17:27:49 +0800

    Update test.cpp

--- 二周目/GA - TSP/test.cpp
+++ 二周目/GA - TSP/test.cpp
@@ -1,16 +1,18 @@
 #include "tsp.h"
 #include <cstdlib>
 #include <iostream>
+#include <math.h>
 #include <vector>
 
 using namespace std;
 
 int main() {
-    const int CITY_DIM = 51;
-    vector<vector<int>> city_loc_vec(CITY_DIM, vector<int>(2));
-    tsp::loadCityLocation(city_loc_vec, CITY_DIM);
-    for (int ci = 0; ci < CITY_DIM; ci++)
-        printf("%d %d\n", city_loc_vec[ci][0], city_loc_vec[ci][1]);
+    tsp::initialize(51);
+    for (int ci = 0; ci < 51; ci++) {
+        for (int cj = 0; cj < 51; cj++)
+            cout << tsp::city_dis_RM[ci][cj] << " ";
+        cout << endl;
+    }
 
     return 0;
 }
