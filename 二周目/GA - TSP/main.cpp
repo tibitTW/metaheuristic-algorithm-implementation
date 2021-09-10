7213cdce6b89fb7a144170078cb2516aac5413f5
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-09-10 14:32:39 +0800

    Create main.cpp

---
+++ 二周目/GA - TSP/main.cpp
@@ -0,0 +1,22 @@
+#include "ga.h"
+#include "tsp.h"
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    const int CITY_DIM = 51;
+    const int ITERATION = 1000;
+    const int POP = 100;
+    const double C_RATE = 0.8;
+    const double M_RATE = 0.2;
+    const int RUN = 51;
+
+    for (int ri = 0; ri < RUN; ri++) {
+        // ga();
+    }
+
+    return 0;
+}
