10f6ff3c64f3a119e73dc84a8f77726c6e8fa0e0
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-09-11 23:18:08 +0800

    add comments

--- 二周目/GA - TSP/config.h
+++ 二周目/GA - TSP/config.h
@@ -1,7 +1,7 @@
 #define CITY_DIM 51 // city dimension
 #define ITERATION 1000
-#define POP 100
-#define C_RATE 0.8
-#define M_RATE 0.1
-#define C_TYPE 1
-#define RUN 51
+#define POP 100    // population
+#define C_RATE 0.8 // crossover rate
+#define M_RATE 0.1 // mutation rate
+#define C_TYPE 1   // crossover type: 0/1/2 <-> PMX/CX/OX
+#define RUN 51     // times of GA() runs
