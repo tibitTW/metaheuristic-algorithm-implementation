5d3ebbdf95051c9a5feab010afcb6ab2a8e48e43
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-09-12 00:55:52 +0800

    Update test.cpp

--- 二周目/GA - TSP/test.cpp
+++ 二周目/GA - TSP/test.cpp
@@ -9,14 +9,7 @@
 using namespace std;
 
 int main() {
-    Chromo c1{1, 3, 9, 7, 5, 4, 6, 2, 8}, c2{4, 6, 2, 1, 7, 8, 9, 3, 5};
-    CX(c1, c2, 9);
-    for (int ci = 0; ci < 9; ci++)
-        cout << c1[ci] << " ";
-    cout << endl;
-    for (int ci = 0; ci < 9; ci++)
-        cout << c2[ci] << " ";
-    cout << endl;
+    //
 
     return 0;
 }
