db9f8f5275970fe77ce8cf07a52987473d6f7729
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-09-13 16:27:22 +0800

    finish OX()

--- v2/GA - TSP/ga.h
+++ v2/GA - TSP/ga.h
@@ -79,6 +79,82 @@ void CX(Chromo &c1, Chromo &c2) {
 // TODO
 // order crossover
 void OX(Chromo &c1, Chromo &c2) {}
+void OX(Chromo &c1, Chromo &c2) {
+    Chromo new_c1, new_c2;
+
+    int flag1 = rand() % CITY_DIM, flag2 = rand() % (CITY_DIM - 1);
+    if (flag1 > flag2) {
+        int tmp = flag1;
+        flag1 = flag2;
+        flag2 = tmp;
+    } else {
+        flag2 += 1;
+    }
+
+    bool is_in_substring;
+    for (int i = flag2 + 1; i < CITY_DIM; i++) {
+        is_in_substring = false;
+        for (int fi = flag1; fi <= flag2; fi++) {
+            if (c1[i] == c2[fi]) {
+                is_in_substring = true;
+                break;
+            }
+        }
+        if (!is_in_substring) {
+            new_c1.push_back(c1[i]);
+        }
+
+        is_in_substring = false;
+        for (int fi = flag1; fi <= flag2; fi++) {
+            if (c2[i] == c1[fi]) {
+                is_in_substring = true;
+                break;
+            }
+        }
+        if (!is_in_substring) {
+            new_c2.push_back(c2[i]);
+        }
+    }
+    for (int i = 0; i < flag1; i++) {
+        is_in_substring = false;
+        for (int fi = flag1; fi <= flag2; fi++) {
+            if (c1[i] == c2[fi]) {
+                is_in_substring = true;
+                break;
+            }
+        }
+        if (!is_in_substring) {
+            new_c1.push_back(c1[i]);
+        }
+        is_in_substring = false;
+        for (int fi = flag1; fi <= flag2; fi++) {
+            if (c2[i] == c1[fi]) {
+                is_in_substring = true;
+                break;
+            }
+        }
+        if (!is_in_substring) {
+            new_c2.push_back(c2[i]);
+        }
+    }
+
+    for (int i = flag1; i <= flag2; i++) {
+        new_c1.push_back(c1[i]);
+        new_c2.push_back(c2[i]);
+    }
+    for (int i = flag1; i <= flag2; i++) {
+        new_c1.push_back(c2[i]);
+        new_c2.push_back(c1[i]);
+    }
+
+    int ci = flag2 + 1;
+    for (int i = 0; i < CITY_DIM; i++) {
+        c1[ci] = new_c1[i];
+        c2[ci] = new_c2[i];
+        ci++;
+        ci = (ci == CITY_DIM) ? 0 : ci;
+    }
+}
 
 void Crossover(Chromo &c1, Chromo &c2) {
     switch (C_TYPE) {
