8fa1763edaac5d5a3d80f0dd2326f4a318a209c6
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-09-13 14:58:52 +0800

    Update config.h

--- v2/GA - TSP/config.h
+++ v2/GA - TSP/config.h
@@ -1,8 +1,10 @@
 #define CITY_DIM 51    // city dimension
 #define ITERATION 1000 // iteration
-#define POP 100        // population
-#define C_RATE 0.8     // crossover rate
-#define M_RATE 0.1     // mutation rate
-#define C_TYPE 1       // crossover type: 1/2/3 <-> PMX/CX/OX
+#define POP 200        // population
+#define C_RATE 0.4     // crossover rate
+#define M_RATE 0.15    // mutation rate
+#define C_TYPE 2       // crossover type: 1/2/3 <-> PMX/CX/OX
 #define S_TYPE 1       // selection type: 1/2 <-> RWS/TS
 #define RUN 51         // times of GA() runs
+#define RESULT_PATH "result.csv"
+#define AVG_51_PATH "51_run_avg.txt"
