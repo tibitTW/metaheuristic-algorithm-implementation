92df8be4e2c5ccbb62995019f862da770e1cf435
Li Chun Hua <jim870718@gmail.com>
2021-10-18 09:07:15 +0800

    de main structure

---
+++ DE/de.hpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using std::vector;
+
+// solution datatype
+typedef vector<double> S;
+
+namespace DE {
+void crossover();
+void mutation();
+void selection();
+void DE();
+} // namespace DE
