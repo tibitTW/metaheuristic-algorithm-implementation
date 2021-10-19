494ce580c64bdbcbb8aafea58b99be0a493856bd
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-10-19 16:22:20 +0800

    test

--- modules/test.cpp
+++ modules/test.cpp
@@ -1,17 +1,19 @@
 #include <ctime>
 #include <iostream>
 #include <random>
+#include <vector>
 
 using namespace std;
 
+typedef vector<int> V;
 int main() {
-    random_device rd;
-    default_random_engine generator(rd());
-    uniform_real_distribution<float> unif(-10.0, 10.0);
-
-    float x;
-    for (int i = 0; i < 1000; i++) {
-        x = unif(generator);
-        cout << x << endl;
+    vector<V> P(4, V(4, 0));
+    for (auto v : P) {
+        cout << v.size() << endl;
+        for (auto c : v)
+            cout << c << "_";
+        cout << endl;
     }
+
+    return 0;
 }
