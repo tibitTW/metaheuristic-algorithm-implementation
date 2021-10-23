b61ccec89e05974456b61621e144387a7c03e577
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-10-23 23:57:48 +0800

    .

--- v3/OMP - HC/hc.hpp
+++ v3/OMP - HC/hc.hpp
@@ -3,21 +3,71 @@
 #include <random>
 #include <vector>
 
-using std::vector;
-
+using namespace std;
 class hc {
     typedef vector<int> result;
+    typedef vector<int> solution;
 
   private:
     int NUM_X_DIM, NUM_MAX_ITER;
+    solution s, v;
+    int f, best_f;
+
+    random_device rd;
+    default_random_engine generator;
+    uniform_int_distribution<int> b_dt;
+
+    int fitness(solution sol) {
+        int f = 0;
+        for (int c : sol)
+            f += c ? 1 : 0;
+        return f;
+    }
+
+    solution nb_previous(solution s) {
+        solution nb(NUM_X_DIM);
+        ;
+        ;
+    }
+    solution nb_next(solution s) {
+        solution nb(NUM_X_DIM);
+        int hold = 1;
+        for (int xi = NUM_X_DIM - 1; xi >= 0; xi--) {
+            nb.at(xi) = (s.at(xi) == hold) ? 0 : 1;
+            hold = (s.at(xi) && hold) ? 1 : 0;
+        }
+        return nb;
+    }
+    solution neighbor(solution s) {
+        solution nb;
+        nb = rand() ? nb_previous(s) : nb_next(s);
+        return nb;
+    }
 
   public:
     hc(int x_dim, int max_iter) {
         NUM_X_DIM = x_dim;
         NUM_MAX_ITER = max_iter;
+        s.assign(NUM_X_DIM, 0);
+        s.assign(NUM_X_DIM, 0);
     }
+
     result run() {
-        for (int ii = 0; ii < NUM_MAX_ITER; ii++) {
+
+        // initialization
+        result res(NUM_MAX_ITER, 1);
+        int ii = 0;
+        b_dt.param(uniform_int_distribution<int>(0, 1).param());
+
+        while (ii < NUM_MAX_ITER) {
+            v = neighbor(s);
+            if (fitness(v) >= fitness(s)) {
+                s = v;
+                best_f = fitness(v);
+            }
+            res.at(ii) = best_f;
         }
+
+        return res;
     }
 };
--- v3/OMP - HC/main.cpp
+++ v3/OMP - HC/main.cpp
@@ -8,23 +8,24 @@
 using namespace std;
 typedef vector<int> result;
 
-void update_sum_result(result sum_res, result res, int x_dim) {
-    for (int xi = 0; xi < x_dim; xi++)
+void update_sum_result(result &sum_res, result res, int max_iter) {
+    for (int xi = 0; xi < max_iter; xi++)
         sum_res.at(xi) += res.at(xi);
 }
 
 int main(int argc, char *argv[]) {
-    int x_dim = atoi(argv[0]), max_iter = atoi(argv[1]), run = atoi(argv[2]);
-    result res, sum_res(x_dim, 0);
+    int x_dim = atoi(argv[1]), max_iter = atoi(argv[2]), run = atoi(argv[3]);
+    result res, sum_res(max_iter, 0);
     hc HC(x_dim, max_iter);
     for (int r = 0; r < run; r++) {
         res = HC.run();
-        update_sum_result(sum_res, res, x_dim);
+        update_sum_result(sum_res, res, max_iter);
     }
 
     fstream file;
-    file.open(argv[3], ios::out | ios::trunc);
+    file.open(argv[4], ios::out | ios::trunc);
     for (int ii = 0; ii < max_iter; ii++) {
         file << (double)sum_res.at(ii) / run << "\n";
     }
+    file.close();
 }
