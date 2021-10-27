72ca2b545ced299fc2ded48f32b351b918a6e5a0
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-10-27 15:51:25 +0800

    .

--- L-SHADE/test.cpp
+++ L-SHADE/test.cpp
@@ -1,5 +1,5 @@
 #include "de.hpp"
-
+// #include
 #include <cstdio>
 #include <ctime>
 #include <iostream>
@@ -8,36 +8,9 @@
 
 using namespace std;
 
-void swap(int &a, int &b) {
-    int tmp = a;
-    a = b;
-    b = tmp;
-}
-int Partition(vector<int> &arr, int front, int end) {
-    int pivot = arr[end];
-    int i = front - 1;
-    for (int j = front; j < end; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
-    i++;
-    swap(arr[i], arr[end]);
-    return i;
-}
-void QuickSort(vector<int> &arr, int front, int end) {
-    if (front < end) {
-        int pivot = Partition(arr, front, end);
-        QuickSort(arr, front, pivot - 1);
-        QuickSort(arr, pivot + 1, end);
-    }
-}
-
 int main() {
-    vector<int> v1 = {19, 10, 8, 5, 6, 15, 12, 9, 3, 16, 11, 17, 2, 18, 4, 1, 13, 20, 7, 14};
-    QuickSort(v1, 0, v1.size() - 1);
-
+    vector<int> v1 = {1, 2, 3, 4, 5};
+    v1.resize(3);
     for (auto i : v1)
         cout << i << " ";
 
