c1b5d94c509d5c73ba999b6c6b4f10eb42ae22f7
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-11-09 15:36:49 +0800

    Update ex.sh

--- L-SHADE/ex.sh
+++ L-SHADE/ex.sh
@@ -4,19 +4,19 @@ result_path="./result.csv"
 rm $result_path
 
 # output csv header
-for ((i = 0; i < 4; i++)); do
-    printf "${d_arr[i]}," >>${result_path}
-done
-printf "\n" >>$result_path
+printf "f_num,10d,30d,50d,100d\n" >>$result_path
 
 # output experiment result
 for ((f_num = 1; f_num <= 30; f_num++)); do
+    printf "${f_num}," >>$result_path
     for ((i = 0; i < 4; i++)); do
         # main.exe x_dim     x_min x_max fun_num run output_file
+        ./main.exe $d_arr[i] -100 100 $f_num 51 ./result/result_${d_arr[i]}D_f${f_num}.txt >>${result_path}
         # ./main.exe $d_arr[i] -100 100 $f_num 1 result_${i}D_f${f_num}.txt >>${result_path}
-        ./main.exe $d_arr[i] -100 100 $f_num 1 "" >>${result_path}
-        printf "," >>$result_path
+
+        if [ "$i" -ne 3 ]; then
+            printf "," >>$result_path
+        fi
     done
     printf "\n" >>$result_path
 done
-printf "\n" >>$result_path
