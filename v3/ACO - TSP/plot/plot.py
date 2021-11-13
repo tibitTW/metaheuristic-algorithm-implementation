d129328d3a8338ff190dab66b9f9f92f94734625
Li Chun Hua <jim870718@gmail.com>
2021-11-13 17:49:03 +0800

    TODO: repair bug (沒計算程式尾到頭長度)

--- v3/ACO - TSP/plot/plot.py
+++ v3/ACO - TSP/plot/plot.py
@@ -4,23 +4,30 @@ import pandas as pd
 
 CITY_DIM = 51
 
+# load locations
+with open("../eil51.txt") as f:
+    locations = f.read()
+
+locations = tuple(
+    tuple(map(int, loc.split(" ")[1:])) for loc in locations.strip().split("\n")
+)
+
 
 def load_city_path(file_path) -> list:
     with open(file_path) as f:
         path = f.read()
-
     path = list(map(int, path.strip().split("\n")))
-
     return path
 
 
 def display_city_location():
-    for loc in locations:
+    for i, loc in enumerate(locations):
         x, y = loc
         plt.scatter(x, y, marker="x", c="k")
+        plt.text(x, y, str(i), size=12, zorder=127)
 
 
-def display_path_graph(path):
+def display_path(path):
     if max(path) == CITY_DIM:
         path = [pi - 1 for pi in path]
 
@@ -34,26 +41,62 @@ def display_path_graph(path):
         xarr, yarr = zip(*(c1, c2))
         plt.plot(xarr, yarr, c="b")
 
+    c1, c2 = locations[path[0]], locations[path[1]]
+    plt.arrow(
+        c1[0],
+        c1[1],
+        c2[0] - c1[0],
+        c2[1] - c1[1],
+        width=0.5,
+        head_width=2,
+        color="r",
+        zorder=63,
+    )
 
-# load locations
-with open("eil51.txt") as f:
-    locations = f.read()
 
-locations = tuple(
-    tuple(map(int, loc.split(" ")[1:])) for loc in locations.strip().split("\n")
-)
+def display_graph(path):
+    if max(path) == CITY_DIM:
+        path = [pi - 1 for pi in path]
+
+    plt.figure(figsize=(12, 12))
+    plt.xlim(-5, 75)
+    plt.ylim(-5, 75)
+
+    for i in range(CITY_DIM):
+        c1 = locations[path[i]]
+        if i == CITY_DIM - 1:
+            c2 = locations[path[0]]
+        else:
+            c2 = locations[path[i + 1]]
+
+        xarr, yarr = zip(*(c1, c2))
+        plt.plot(xarr, yarr, c="b")
+
+    display_city_location()
+
+    c1, c2 = locations[path[0]], locations[path[1]]
+    plt.arrow(
+        c1[0],
+        c1[1],
+        c2[0] - c1[0],
+        c2[1] - c1[1],
+        width=0.5,
+        head_width=2,
+        color="r",
+        zorder=63,
+    )
+    plt.show()
 
 
 # %%
 path_opt = load_city_path("./path/path_opt.txt")
+display_graph(path_opt)
 
-plt.figure(figsize=(10, 10))
-plt.xlim(0, 70)
-plt.ylim(0, 70)
+# %%
+with open("./path/paths.txt") as f:
+    paths = f.read()
+paths = [tuple(map(int, p.split("\n"))) for p in paths.strip().split("\n\n")]
 
-display_path_graph(path_opt)
-display_city_location()
-
-plt.show()
+display_graph(paths[-1])
 
 # %%
