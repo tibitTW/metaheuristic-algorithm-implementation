6224bdad7dd2c225ad66ce8124d409d9ba72b78b
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-11-17 18:27:49 +0800

    Update ex.sh

--- L-SHADE/ex.sh
+++ L-SHADE/ex.sh
@@ -3,18 +3,18 @@ result_path="./result.csv"
 rm ${result_path}
 
 # output csv header
-# printf "f_num,10d,30d,50d,100d\n" >>$result_path
-printf "f_num,10d\n" >>$result_path
+printf "f_num,10d,30d,50d,100d\n" >>$result_path
+# printf "f_num,10d\n" >>$result_path
 
 # output experiment result
 for ((f_num = 1; f_num <= 30; f_num++)); do
     printf "${f_num}," >>$result_path
     ./main.exe 10 -100 100 $f_num 51 ./result/result_10D_f${f_num}.txt >>${result_path}
-    # printf "," >>$result_path
-    # ./main.exe 30 -100 100 $f_num 51 ./result/result_30D_f${f_num}.txt >>${result_path}
-    # printf "," >>$result_path
-    # ./main.exe 50 -100 100 $f_num 51 ./result/result_50D_f${f_num}.txt >>${result_path}
-    # printf "," >>$result_path
-    # ./main.exe 100 -100 100 $f_num 51 ./result/result_100D_f${f_num}.txt >>${result_path}
+    printf "," >>$result_path
+    ./main.exe 30 -100 100 $f_num 51 ./result/result_30D_f${f_num}.txt >>${result_path}
+    printf "," >>$result_path
+    ./main.exe 50 -100 100 $f_num 51 ./result/result_50D_f${f_num}.txt >>${result_path}
+    printf "," >>$result_path
+    ./main.exe 100 -100 100 $f_num 51 ./result/result_100D_f${f_num}.txt >>${result_path}
     printf "\n" >>$result_path
 done
